27a3e490520b9077c638439dff4fd82b08031d14
JF Rene <renejf@gmail.com>
2026-09-16 03:16:18 -0400

    fix: heroAnimName returns 'supermove' during dash

--- petal-panic/js/systems/render.js
+++ petal-panic/js/systems/render.js
@@ -347,6 +347,7 @@ export function render(ctx) {
  */
 export function heroAnimName(h) {
   if (h.dying) return 'dead';
+  if (h.superActive) return 'supermove';
   if (h.crouching) return 'crouch';
   if (h.meleeFrame > 0) return 'melee';
   if (h.jumpsUsed > 0) return h.jumpsUsed >= 2 ? 'djump' : 'jump';
